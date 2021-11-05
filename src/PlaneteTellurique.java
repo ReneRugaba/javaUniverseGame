df8a6b291f1e8f495f5ed192f831feb83da928fd
rugaba <rugaba@gmail.com>
2021-11-05 15:47:49 +0100

    arrayList

--- src/PlaneteTellurique.java
+++ src/PlaneteTellurique.java
@@ -1,21 +1,30 @@
 public class PlaneteTellurique extends Planete {
     int totalVisiteur;
-    Vaisseau[] vaisseauAccoste;
+    Vaisseau[][] vaisseauAccoste;
 
     PlaneteTellurique(String nomPlanete,int nbPlaceBaie) {
         super(nomPlanete);
-        this.vaisseauAccoste= new Vaisseau[nbPlaceBaie];
+        this.vaisseauAccoste= new Vaisseau[2][nbPlaceBaie];
     }
 
     void incrementTotalVisiteur (int nbPassager){
         this.totalVisiteur+=nbPassager;
     }
 
-    boolean restePlaceLibre(){
-        for (Vaisseau v : this.vaisseauAccoste){
-            if(v==null){
-                return true;
-            }
+    boolean restePlaceLibre(Vaisseau vaisseau){
+        int indexZone=0;
+        switch (vaisseau.typeVaisseau){
+            case CARGO:
+            case VAISSEAU_MONDE:
+                indexZone=1;
+        }
+        for (int i=0;i<this.vaisseauAccoste[indexZone].length;i++){
+
+                if(vaisseauAccoste[indexZone][i]==null){
+                    return true;
+                }
+
+
         }
         return false;
     }
@@ -27,41 +36,44 @@ public class PlaneteTellurique extends Planete {
             }
 
 
-            boolean placeLibre = this.restePlaceLibre();
+            boolean placeLibre = this.restePlaceLibre(vs);
 
             if (!placeLibre){
                 System.out.println("La baie d'accostage ne dispose plus de place libre!");
             }else {
+                int indexZone=0;
                 switch (vs.typeVaisseau){
                     case FREGATE:
-                        this.incrementTotalVisiteur(vs.nbPassager);
-                        break;
                     case CROISEUR:
-                        this.incrementTotalVisiteur(vs.nbPassager);
-                        break;
                     case CHASSEUR:
                         this.incrementTotalVisiteur(vs.nbPassager);
                         break;
                     case CARGO:
-                        this.incrementTotalVisiteur(vs.nbPassager);
-                        break;
                     case VAISSEAU_MONDE:
+                        indexZone=1;
                         this.incrementTotalVisiteur(vs.nbPassager);
                         break;
                     default:
                         throw new IllegalStateException("Unexpected value: " + vs.typeVaisseau);
                 }
 
-                for (int i=0;i<this.vaisseauAccoste.length;i++){
-                    if (this.vaisseauAccoste[i]==null){
-                        this.vaisseauAccoste[i]=vs;
-                        System.out.println("La baie vient d'accueillir un vaisseau de type "+vs.typeVaisseau);
-                        break;
+
+
+                    for (int j=0;j<this.vaisseauAccoste[indexZone].length;j++){
+                        if (vs instanceof VaisseauGuerre && this.vaisseauAccoste[indexZone][j]==null){
+                            this.vaisseauAccoste[indexZone][j]=vs;
+                            System.out.println("La baie pour les vaisseau de guerre vient d'accueillir un vaisseau de type "+vs.typeVaisseau);
+                            break;
+                        }else if(vs instanceof VaisseauCivil && this.vaisseauAccoste[indexZone][j]==null) {
+                            this.vaisseauAccoste[indexZone][j]=vs;
+                            System.out.println("La baie pour les vaisseau civiles vient d'accueillir un vaisseau de type "+vs.typeVaisseau);
+                            break;
+                        }
                     }
+
                 }
             }
 
         }
 
-    }
 }
